6f1887155db9174b2047018cab36cb4dac44fb03
brianalexa <117391783+brianalexa@users.noreply.github.com>
2022-11-05 13:55:14 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -883,7 +883,7 @@ export async function handler(chatUpdate) {
                 if (!('sBye' in chat)) chat.sBye = ''
                 if (!('sPromote' in chat)) chat.sPromote = ''
                 if (!('sDemote' in chat)) chat.sDemote = ''
-                if (!('delete' in chat)) chat.delete = true
+                if (!('delete' in chat)) chat.delete = false
                 if (!('modohorny' in chat)) chat.modohorny = false    
                 if (!('autosticker' in chat)) chat.autosticker = false                    
                 if (!('audios' in chat)) chat.audios = false                            
@@ -904,7 +904,7 @@ export async function handler(chatUpdate) {
                     sBye: '',
                     sPromote: '',
                     sDemote: '',
-                    delete: true,
+                    delete: false,
                     modohorny: false,
                     autosticker: false,
                     audios: true,
